a7e3479fd17704c23d2088fb430109efcf96b48f
“niharr05” <ramoliya.nihar05@gmail.com>
2026-09-18 16:37:05 +0530

    Solve LeetCode problems in C++

---
+++ 53  Maximum Subarray.cpp
@@ -0,0 +1,20 @@
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+  int maxSubArray(std::vector<int> &nums) {
+    int max_sum = nums[0];
+    int current_sum = nums[0];
+
+    for (size_t i = 1; i < nums.size(); ++i) {
+      // Decide to extend current subarray or start fresh from nums[i]
+      current_sum = std::max(nums[i], current_sum + nums[i]);
+
+      // Keep track of maximum sum found so far
+      max_sum = std::max(max_sum, current_sum);
+    }
+
+    return max_sum;
+  }
+};
